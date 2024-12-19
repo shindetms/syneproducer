9284be5a14b5c340c56ff78f14162b56ea513326
shindetms <tusharshindetms@gmail.com>
2024-12-19 07:45:14 +0000

    first commit

---
+++ src/main/java/com/syneproducer/SyneproducerApplication.java
@@ -0,0 +1,15 @@
+package com.syneproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SyneproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyneproducerApplication.class, args);
+	}
+
+}
